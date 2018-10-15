db7d053e43318c5562c835762337854e6455211f
wangxiaobai <937057445@qq.com>
2018-10-15 20:04:03 +0800

    HAHA

--- shatuzi.py
+++ shatuzi.py
@@ -1,23 +1,11 @@
 print('可爱的小白兔')
 
-<<<<<<< HEAD
-
 print('小了白了兔')
 
 print('小了白了兔')
-=======
-<<<<<<< HEAD
-print('我是兔子')
-=======
-<<<<<<< HEAD
-x = 13
-=======
-x = 14
->>>>>>> f71564112a7a645079112f903444da417d186644
->>>>>>> 22ed3be1043517bd1e7c8074671ca2bfd484a696
 
 
-hello = "oooooo"
-hello = "java"
+
+hello = "ooooo
 
 
